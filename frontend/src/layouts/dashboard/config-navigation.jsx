bb4dbe8b9e3f30dedb5f631256324d4f13a8bac7
Omelettia <taikhoanguyen@tutanota.com>
2024-05-26 23:26:21 +0700

    frontend

---
+++ frontend/src/layouts/dashboard/config-navigation.jsx
@@ -0,0 +1,37 @@
+import SvgColor from 'src/components/svg-color';
+
+// ----------------------------------------------------------------------
+
+const icon = (name) => (
+  <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />
+);
+
+const navConfig = [
+  {
+    title: 'dashboard',
+    path: '/',
+    icon: icon('ic_analytics'),
+  },
+  {
+    title: 'user',
+    path: '/user',
+    icon: icon('ic_user'),
+  },
+  {
+    title: 'equipments',
+    path: '/equipments',
+    icon: icon('ic_cart'),
+  },
+  {
+    title: 'bookings',
+    path: '/blog',
+    icon: icon('ic_blog'),
+  },
+  {
+    title: 'login',
+    path: '/login',
+    icon: icon('ic_lock'),
+  },
+];
+
+export default navConfig;
